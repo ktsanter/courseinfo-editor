0160c5b4ec3e405bac557cb32add132e1f985ba4
SpunkyAardvark123 <ktsanter@gmail.com>
2018-09-21 12:18:17 -0400

    selection and display working

--- js/mapper.js
+++ js/mapper.js
@@ -1,4 +1,9 @@
 //
+// TODO: add retrieval and dropdown for course list
+// TODO: initialize settings.selectedItems based on course selection
+// TODO: add save for mappings per course
+// TODO: move some more CSS to common?
+// TODO: consider way to reorder list and have it preserved between tree selections
 //
 const app = function () {
 	const PAGE_TITLE = 'Course info mapper'
@@ -7,7 +12,8 @@ const app = function () {
 		body: null,
 		notice: null,
 		title: null,
-		itemtree: null
+		itemtree: null,
+		mapspace: null
 	};
 	
 	const settings = {
@@ -27,12 +33,15 @@ const app = function () {
 		page.title = document.getElementById('title');
 
 		page.itemtree = $('#tree1');
-						
+		page.mapspace = document.getElementById('mapspace');
+		
 		page.body.classList.add('cif-colorscheme');
 		page.header.classList.add('cif-title');
 		page.notice.classList.add('cif-notice');			
 		
 		page.header.toolname.innerHTML = PAGE_TITLE;
+		
+		settings.selectedItems = [];
 		
 		_setNotice('initializing...');
 		if (!_initializeSettings()) {
@@ -86,12 +95,79 @@ result = true;
 			slide: false
 		});
 		
-//		page.itemtree.on('tree.select', function(e) {_treeSelectHandler(e);} );
 		page.itemtree.on('tree.click', function(e) { _treeClickHandler(e);} );
 	}
 	
 	function _renderSelectedItems() {
-		console.log('render items');
+		_updateSelectedItemList();
+		
+		var elemWrapper = page.mapspace;
+		while (elemWrapper.childNodes.length > 0) {
+			elemWrapper.removeChild(elemWrapper.childNodes[0]);
+		}
+
+		for (var i = 0; i < settings.selectedItems.length; i++) {
+			_renderItem(settings.selectedItems[i]);
+		}
+		
+		_addAccordionHandlers();
+	}
+		
+	function _renderItem(item) {
+		var elemLabel = document.createElement('button');
+		elemLabel.classList.add('ci-accordion');
+		elemLabel.innerHTML = item.content.label;
+		
+		var elemInfo = document.createElement('div');
+		elemInfo.classList.add('ci-panel');
+		elemInfo.innerHTML = formatTextFromMarkup(item.content.markdown, false);
+		
+		page.mapspace.appendChild(elemLabel);
+		page.mapspace.appendChild(elemInfo);
+	}
+	
+	function _addAccordionHandlers() {
+		var acc = document.getElementsByClassName("ci-accordion");
+		var i;
+
+		for (i = 0; i < acc.length; i++) {
+			if (acc[i].id != 'has_handler') {
+				acc[i].id = 'has_handler';  // avoid duplicate handlers
+
+				acc[i].addEventListener("click", function(evt) {
+					// toggle active state for this panel and set display accordingly
+					this.classList.toggle("ci-active");
+					
+					panel = this.nextElementSibling;
+					if (panel.style.display === "block") {
+						panel.style.display = "none";
+					} else {
+						panel.style.display = "block";
+					}
+
+					// hide contents of any other open panels
+					var activePanels = document.getElementsByClassName("ci-active");
+					for (var j = 0; j < activePanels.length; j++) {
+						var activePanel = activePanels[j];
+						if (activePanel != this) {
+							activePanel.classList.remove("ci-active");
+							activePanel.nextElementSibling.style.display = "none";
+						}
+					}
+				});
+			}
+		}
+	}
+
+	function _updateSelectedItemList() {
+		var list = settings.selectedItems;
+		
+		var root = page.itemtree.tree('getTree');
+		var treelist = _getSelectedNodes(root);	
+		
+		list = treelist;  // make deltas to list based on treelist first?
+		
+		settings.selectedItems = list;
 	}
 	
 	//------------------------------------------------------------------
@@ -125,6 +201,23 @@ result = true;
 		for (var i = 0; i < children.length; i++) {
 			_propagateSelection(children[i], makeSelected);
 		}
+	}
+	
+	function _getSelectedNodes(basenode) {
+		var list = [];
+		
+		if (basenode.hasOwnProperty('content')) {
+			if (_isLeaf(basenode) && page.itemtree.tree('isNodeSelected', basenode)) {
+				list.push(basenode);
+			}
+		}
+		
+		var children = basenode.children;
+		for (var i = 0; i < children.length; i++) {
+			 list = list.concat(_getSelectedNodes(children[i]));
+		}
+			
+		return list;
 	}
 	
 	//---------------------------------------
